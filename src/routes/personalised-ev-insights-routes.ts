dc9ad8aa151035704fef6a445e810190000de7b5
jezzif <jezzif12@gmail.com>
2026-05-01 14:02:51 +1000

    Added swagger docs

--- src/routes/personalised-ev-insights-routes.ts
+++ src/routes/personalised-ev-insights-routes.ts
@@ -14,10 +14,323 @@ const personalisedEVInsightsController = new PersonalisedEVInsightsController(
   personalisedEVInsightsService
 );
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *    InsightsResponse:
+ *      type: object
+ *      properties:
+ *        userId:
+ *          type: string
+ *        email:
+ *          type: string
+ *        weekly_km:
+ *          type: number
+ *        trip_length:
+ *          type: string
+ *        driving_frequency:
+ *          type: string
+ *        driving_type:
+ *          type: string
+ *        road_trips:
+ *          type: string
+ *        car_ownership:
+ *          type: string
+ *        fuel_efficiency:
+ *          type: number
+ *        monthly_fuel_spend:
+ *          type: number
+ *        home_charging:
+ *          type: string
+ *        solar_panels:
+ *          type: string
+ *        charging_preference:
+ *          type: string
+ *        budget:
+ *          type: string
+ *        priorities:
+ *          type: string
+ *        postcode:
+ *          type: string
+ *        cluster:
+ *          type: number
+ *        profile_type:
+ *          type: string
+ *        description:
+ *          type: string
+ *        estimatedSavings:
+ *          type: number
+ *        savingsMessage:
+ *          type: string
+ *        similarDriverAverages:
+ *          type: object
+ *          properties:
+ *            weekly_km:
+ *              type: number
+ *            fuel_efficiency:
+ *              type: number
+ *            monthly_fuel_spend:
+ *              type: number
+ *        allDriverAverages:
+ *          type: object
+ *          properties:
+ *            weekly_km:
+ *              type: number
+ *            fuel_efficiency:
+ *              type: number
+ *            monthly_fuel_spend:
+ *              type: number
+ *        comparison:
+ *          type: object
+ *          properties:
+ *            sim_weekly_km_difference:
+ *              type: number
+ *            sim_fuel_efficiency_difference:
+ *              type: number
+ *            sim_monthly_fuel_spend_difference:
+ *              type: number
+ *            all_weekly_km_difference:
+ *              type: number
+ *            all_fuel_efficiency_difference:
+ *              type: number
+ *            all_monthly_fuel_spend_difference:
+ *              type: number
+ * 
+ */
+/**
+ * @swagger
+ * /api/personalised-ev-insights:
+ *  post:
+ *    tags:
+ *      - Insights
+ *    summary: Submit insights
+ *    description: Submit a new insights form
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            required:
+ *              - userId 
+ *              - email
+ *              - weekly_km
+ *              - trip_length
+ *              - driving_frequency
+ *              - driving_type
+ *              - road_trips
+ *              - car_ownership
+ *              - fuel_efficiency
+ *              - monthly_fuel_spend
+ *              - home_charging
+ *              - solar_panels
+ *              - charging_preference
+ *              - budget
+ *              - priorities
+ *              - postcode
+ *            properties:
+ *              userId:
+ *                type: string
+ *                maxLength: 24
+ *              email:
+ *                type: string
+ *                format: email
+ *                example: "johndoe@example.com"
+ *                maxLength: 255
+ *              weekly_km:
+ *                type: number
+ *                example: 250
+ *                minimum: 0
+ *              trip_length:
+ *                type: string
+ *                maxLength: 48
+ *              driving_frequency:
+ *                type: string
+ *                maxLength: 48
+ *              driving_type:
+ *                type: string
+ *                maxLength: 100
+ *              road_trips:
+ *                type: string
+ *                maxLength: 3
+ *              car_ownership:
+ *                type: string
+ *                maxLength: 22
+ *              fuel_efficiency:
+ *                type: number
+ *                minimum: 0 
+ *              monthly_fuel_spend:
+ *                type: number
+ *                minimum: 0
+ *              home_charging:
+ *                type: string
+ *                maxLength: 3
+ *              solar_panels:
+ *                type: string
+ *                maxLength: 3
+ *              charging_preference:
+ *                type: string
+ *                maxLength: 16
+ *              budget:
+ *                type: string
+ *                maxLength: 32
+ *              priorities:
+ *                type: string
+ *                maxLength: 255
+ *              postcode:
+ *                type: string
+ *                example: "3000"
+ *                maxLength: 4
+ *    responses:
+ *      201:
+ *        description: Insights submitted successfully
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "Personalised EV insight generated and saved successfully"
+ *                data:
+ *                  $ref: '#/components/schemas/InsightsResponse'   
+ *      400:
+ *        description: Bad request - validation error
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *      404:
+ *        description: Bad request - user not found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ * 
+ */
 router.post("/", authGuard(["user", "admin"]), (req, res) =>
   personalisedEVInsightsController.submitInsights(req, res)
 );
 
+/**
+ * @swagger
+ * /api/personalised-ev-insights/latest:
+ *  get:
+ *    tags:
+ *      - Insights
+ *    summary: get EV Usage Insights
+ *    description: Get EV Usage Insights based on latest form submission
+ *    security:
+ *      - bearerAuth: []
+ *    responses:
+ *      200:
+ *        description: Insight retrieved successfully
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "success"
+ *                data:
+ *                  type: object
+ *                  properties:
+ *                    userId:
+ *                      type: string
+ *                    email:
+ *                      type: string
+ *                    weekly_km:
+ *                      type: number
+ *                    trip_length:
+ *                      type: string
+ *                    driving_frequency:
+ *                      type: string
+ *                    driving_type:
+ *                      type: string
+ *                    road_trips:
+ *                      type: string
+ *                    car_ownership:
+ *                      type: string
+ *                    fuel_efficiency:
+ *                      type: number
+ *                    monthly_fuel_spend:
+ *                      type: number
+ *                    home_charging:
+ *                      type: string
+ *                    solar_panels:
+ *                      type: string
+ *                    charging_preference:
+ *                      type: string
+ *                    budget:
+ *                      type: string
+ *                    priorities:
+ *                      type: string
+ *                    postcode:
+ *                      type: string
+ *                    cluster:
+ *                      type: number
+ *                    profile_type:
+ *                      type: string
+ *                    description:
+ *                      type: string
+ *                    estimatedSavings:
+ *                      type: number
+ *                    savingsMessage:
+ *                      type: string
+ *                    similarDriverAverages:
+ *                      type: object
+ *                      properties:
+ *                        weekly_km:
+ *                          type: number
+ *                        fuel_efficiency:
+ *                          type: number
+ *                        monthly_fuel_spend:
+ *                          type: number
+ *                    allDriverAverages:
+ *                      type: object
+ *                      properties:
+ *                        weekly_km:
+ *                          type: number
+ *                        fuel_efficiency:
+ *                          type: number
+ *                        monthly_fuel_spend:
+ *                          type: number
+ *                    comparison:
+ *                      type: object
+ *                      properties:
+ *                        sim_weekly_km_difference:
+ *                          type: number
+ *                        sim_fuel_efficiency_difference:
+ *                          type: number
+ *                        sim_monthly_fuel_spend_difference:
+ *                          type: number
+ *                        all_weekly_km_difference:
+ *                          type: number
+ *                        all_fuel_efficiency_difference:
+ *                          type: number
+ *                        all_monthly_fuel_spend_difference:
+ *                          type: number
+ *      404:
+ *        description: Bad request -  No insights found for user
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example: "No personalised EV insights found for this user"
+ *      400:
+ *        description: Unauthorised
+ */
 router.get("/latest", authGuard(["user", "admin"]), (req, res) =>
   personalisedEVInsightsController.getMyInsights(req, res)
 );
--- src/routes/weather-aware-routing-routes.ts
+++ src/routes/weather-aware-routing-routes.ts
@@ -3,6 +3,109 @@ import WeatherAwareRoutingController from "../controllers/weather-aware-routing-
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     WeatherAwareRoutingResponse:
+ *       type: object
+ *       properties:
+ *         prediction:
+ *           type: number
+ *         dist_to_nearest_ev_m:
+ *           type: number
+ *         ev_within_500m:
+ *           type: number
+ *         avg_temp:
+ *           type: number
+ *         total_prcp:
+ *           type: number
+ *         used_SHAPE_Length:
+ *           type: number
+ *     WeatherAwareRoutingLocation:
+ *       type: object
+ *       properties:
+ *         year:
+ *           type: number
+ *         start_lat:
+ *           type: number
+ *         start_lon:
+ *           type: number
+ */
+/**
+ * @swagger
+ * /api/weather-aware-routing/predict:
+ *   post:
+ *     tags:
+ *       - weatherAwareRouting
+ *     summary:
+ *       Predict energy consumption
+ *     description:
+ *       Predicts energy consumption of driving to location and other data
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - year
+ *               - start_lat
+ *               - start_lon
+ *             properties:
+ *               year:
+ *                 type: number
+ *                 example: 2023
+ *                 maxLength: 4
+ *               start_lat:
+ *                 type: number
+ *                 example: -37.814
+ *                 maxLength: 16
+ *               start_lon:
+ *                 type: number
+ *                 example: 144.96205
+ *                 maxLength: 16
+ *     responses:
+ *       400:
+ *         description: Bad request - validation error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "year, start_lat and start_lon are required"
+ *       201:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 prediction:
+ *                   type: number
+ *                 dist_to_nearest_ev_m:
+ *                   type: number
+ *                 ev_within_500m:
+ *                   type: number
+ *                 avg_temp:
+ *                   type: number
+ *                 total_prcp:
+ *                   type: number
+ *                 used_SHAPE_Length:
+ *                   type: number
+ *       500:
+ *         description: Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Internal server error"
+ */
 router.post("/predict", WeatherAwareRoutingController.predict);
 
 export default router;
